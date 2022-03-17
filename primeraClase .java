c8f7461f267d709da72474a9185718c38ca55d27
serac8 <92148837+serac8@users.noreply.github.com>
2022-03-17 14:21:19 +0100

    commit de prueba

--- primeraClase .java
+++ primeraClase .java
@@ -1,7 +1,7 @@
 public class primeraClase {
 
 	public static void main(String[] args) {
-		System.out.println("Hola");
+		System.out.println("Hofgkfla");
         System.out.println("Creado desde Visual Studio Code");
 	}
 
